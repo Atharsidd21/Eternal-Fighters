4f563adabca7c5efa59b3a6af42de062fd7c1eac
Athar Siddiqui <atharsiddiqui211995@gmail.com>
2024-10-26 19:21:35 +0530

    .

--- Assets/Scrpts/Fight Controller/FightingController.cs
+++ Assets/Scrpts/Fight Controller/FightingController.cs
@@ -3,10 +3,18 @@ using UnityEngine;
 
 public class FightingController : MonoBehaviour
 {
+   [Header("Player Movement")]
    public float moveSpeed = 5f;
    public float rotationSpeed = 10f;
    private CharacterController characterController;
    private Animator animator;
+
+   [Header("Player Fight")]
+   public float attackCooldown = 0.5f;
+   public int attackDamage = 5;
+   public string [] attackAnimations ={"Attack1Animation","Attack2Animation","Attack3Animation","Attack4Animation"};
+   public float dodgeDistance =2f;
+   private float lastataackTime;
    void Awake()
    {
     characterController = GetComponent<CharacterController>();
@@ -15,6 +23,23 @@ public class FightingController : MonoBehaviour
    void Update()
    {
     PerformMovement ();
+    Dodge();
+;     if(Input.GetKeyDown(KeyCode.A))
+     {
+        AttackMehtod(0);
+     }
+     else if(Input.GetKeyDown(KeyCode.S))
+     {
+        AttackMehtod(1);
+     }
+      else if(Input.GetKeyDown(KeyCode.Z))
+     {
+        AttackMehtod(2);
+     }
+      else if(Input.GetKeyDown(KeyCode.X))
+     {
+        AttackMehtod(3);
+     }
    }
 
    void PerformMovement (){
@@ -48,5 +73,35 @@ public class FightingController : MonoBehaviour
 
      characterController.Move(movement * moveSpeed *Time.deltaTime);
 
+   }
+   void AttackMehtod( int attackIndex)
+   {
+       if(Time.time - lastataackTime > attackCooldown)
+       {
+        animator.Play(attackAnimations[attackIndex]);
+
+        int damage = attackDamage;
+        Debug.Log("damage" + (attackIndex+1) + damage);
+
+        lastataackTime = Time.time;
+
+        //looping through opponents
+
+       }
+       else 
+       {
+        Debug.Log("Caannot perform attack yet.Cooldown time remaining.");
+       }
+   }
+   void Dodge()
+   {
+    if(Input.GetKeyDown(KeyCode.D))
+    {
+        animator.Play("DodgeFrontAnimation");
+
+        Vector3 dodgeDirection = transform.forward * dodgeDistance;
+
+        characterController.Move(dodgeDirection);
+    }
    }
 }
